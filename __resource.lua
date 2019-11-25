019e2fae67fb947903ffe4246f4b5a11d52588de
ArkSeyonet <arkseyonet@gmail.com>
2019-11-25 09:29:53 -0600

    Increment Version / Remove Unecessary Files

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX AIO Menu'
 
-version '3.0.0_PRE'
+version '3.0.0'
 
 server_scripts {
 	'@mysql-async/lib/MySQL.lua',
