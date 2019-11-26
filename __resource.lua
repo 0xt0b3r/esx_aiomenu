9be7a3235c366c5231376c848134c7a6abd164d9
ArkSeyonet <31637812+ArkSeyonet@users.noreply.github.com>
2019-11-25 20:09:01 -0600

    Increment Version

--- __resource.lua
+++ __resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'ESX AIO Menu'
 
-version '3.0.0'
+version '3.0.1'
 
 server_scripts {
     '@mysql-async/lib/MySQL.lua',
